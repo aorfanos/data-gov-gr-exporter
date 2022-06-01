d5bbdb79da9e0382953b3b39c0c7c9e96deb3c66
Alex Orfanos <me@aorfanos.com>
2022-06-01 10:01:29 +0300

    add horizontal owners and confiscations

--- gov-gr-exporter/property/collector.go
+++ gov-gr-exporter/property/collector.go
@@ -9,20 +9,28 @@ import (
 
 type PropertyOwnersCollector struct {
 	OwnerCount *prometheus.Desc
+	HorizontalOwnerCount *prometheus.Desc
+	ConfiscationsCount *prometheus.Desc
 }
 
 func (c *PropertyOwnersCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- c.OwnerCount
+	ch <- c.HorizontalOwnerCount
+	ch <- c.ConfiscationsCount
 }
 
 func NewPropertyOwnersCollector() *PropertyOwnersCollector {
 	return &PropertyOwnersCollector{
 		OwnerCount: prometheus.NewDesc("owners_per_region_count", "Count of owners per OTA", []string{"ota_id","ota_full_name","ota_name_en","timestamp_human"}, nil),
+		HorizontalOwnerCount: prometheus.NewDesc("horizontal_owners_per_region_count", "Count of horizontal owners per OTA", []string{"ota_id","ota_full_name","ota_name_en","timestamp_human"}, nil),
+		ConfiscationsCount: prometheus.NewDesc("confiscations_per_region_count", "Confiscations per OTA", []string{"ota_id","ota_full_name","ota_name_en","timestamp_human"}, nil),
 	}
 }
 
 func (c *PropertyOwnersCollector) Collect(ch chan<- prometheus.Metric) {
 	ownerInfo := GetPropertyOwnersPerOta("2022-05-25", "2022-05-26")
+	horizontalOwnerInfo := GetPropertyOwnersPerOta("2022-05-25", "2022-05-26")
+	confiscations := GetConfiscationsPerOta("2022-05-29", "2022-05-31")
 
 	for _, owners := range *ownerInfo {
 		ch <- prometheus.NewMetricWithTimestamp(utils.DateStringToTimePlainDate(owners.Date), prometheus.MustNewConstMetric(
@@ -30,4 +38,21 @@ func (c *PropertyOwnersCollector) Collect(ch chan<- prometheus.Metric) {
 			owners.OwnersCount, strconv.Itoa(owners.OtaId), utils.GreekToLatin(owners.OtaFullName), owners.OtaNameEng, owners.Date))
 	}
 	
+	for _, horzOwners := range *horizontalOwnerInfo {
+		ch <- prometheus.NewMetricWithTimestamp(
+			utils.DateStringToTimePlainDate(horzOwners.Date),
+			prometheus.MustNewConstMetric(
+				c.HorizontalOwnerCount, prometheus.GaugeValue,
+				horzOwners.OwnersCount, strconv.Itoa(horzOwners.OtaId), utils.GreekToLatin(horzOwners.OtaFullName), horzOwners.OtaNameEng, horzOwners.Date),
+			)
+	}
+
+	for _, confs := range *confiscations {
+		ch <- prometheus.NewMetricWithTimestamp(
+			utils.DateStringToTimePlainDate(confs.Date),
+			prometheus.MustNewConstMetric(
+				c.ConfiscationsCount, prometheus.GaugeValue,
+				confs.Confiscations, strconv.Itoa(confs.OtaId), utils.GreekToLatin(confs.OtaFullName), confs.OtaNameEng, confs.Date),
+			)
+	}
 }
--- gov-gr-exporter/property/property.go
+++ gov-gr-exporter/property/property.go
@@ -8,7 +8,9 @@ import (
 	"github.com/aorfanos/gov-gr-exporter/utils"
 )
 
-const ApiEndpoint string = "https://data.gov.gr/api/v1/query/ktm_owners"
+const ApiEndpointOwnersPerOta string = "https://data.gov.gr/api/v1/query/ktm_owners"
+const ApiEndpointHorizontalOwners string = "https://data.gov.gr/api/v1/query/ktm_hplots"
+const ApiEndpointConfiscations string = "https://data.gov.gr/api/v1/query/ktm_confs"
 
 type PropertyOwnersPerMunicipality []struct {
 	OtaId int `json:"ota_id"`
@@ -18,8 +20,16 @@ type PropertyOwnersPerMunicipality []struct {
 	Date string `json:"date"`
 }
 
+type PropertyConfiscationsPerMunicipality []struct {
+	OtaId int `json:"ota_id"`
+	Confiscations float64 `json:"confiscations"`
+	OtaFullName string `json:"ota_full_name"`
+	OtaNameEng string `json:"ota_name_en"` // as provided by data.gov.gr
+	Date string `json:"date"`
+}
+
 func GetPropertyOwnersPerOta(dateFrom, dateTo string) *PropertyOwnersPerMunicipality {
-	body, err := utils.NewGovGrGetRequest(ApiEndpoint+"?date_from="+dateFrom+"&date_to="+dateTo)
+	body, err := utils.NewGovGrGetRequest(ApiEndpointOwnersPerOta+"?date_from="+dateFrom+"&date_to="+dateTo)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -32,4 +42,32 @@ func GetPropertyOwnersPerOta(dateFrom, dateTo string) *PropertyOwnersPerMunicipa
 	// fmt.Println(owners)
 
 	return &owners
+}
+
+func GetHorizontalPropertyOwnersPerOta(dateFrom, dateTo string) *PropertyOwnersPerMunicipality {
+	body, err := utils.NewGovGrGetRequest(ApiEndpointHorizontalOwners+"?date_from="+dateFrom+"&date_to="+dateTo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var owners PropertyOwnersPerMunicipality
+	if err := json.Unmarshal(body, &owners); err != nil {
+		log.Fatal(err)
+	}
+
+	return &owners
+}
+
+func GetConfiscationsPerOta(dateFrom, dateTo string) *PropertyConfiscationsPerMunicipality {
+	body, err := utils.NewGovGrGetRequest(ApiEndpointConfiscations+"?date_from="+dateFrom+"&date_to="+dateTo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var confiscations PropertyConfiscationsPerMunicipality
+	if err := json.Unmarshal(body, &confiscations); err != nil {
+		log.Fatal(err)
+	}
+
+	return &confiscations
 }
