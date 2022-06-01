b295228cd54e6c87b2d4d840ed2a9da7ff7666f7
Alex Orfanos <me@aorfanos.com>
2022-06-02 01:20:19 +0300

    enable collectors

--- gov-gr-exporter/main.go
+++ gov-gr-exporter/main.go
@@ -4,18 +4,28 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/aorfanos/gov-gr-exporter/energy"
 	"github.com/aorfanos/gov-gr-exporter/property"
+	"github.com/aorfanos/gov-gr-exporter/sailing"
+	"github.com/aorfanos/gov-gr-exporter/traffic"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
-	// trafficCollector := traffic.NewAthensTrafficCollector()
+	trafficCollector := traffic.NewAthensTrafficCollector()
 	ownersCollector := property.NewPropertyOwnersCollector()
+	energyCollector := energy.NewRenewableEnergyCollector()
+	sailingCollector := sailing.NewSailingCollector()
 
-	// prometheus.MustRegister(trafficCollector)
+
+	prometheus.MustRegister(trafficCollector)
 	prometheus.MustRegister(ownersCollector)
+	prometheus.MustRegister(energyCollector)
+	prometheus.MustRegister(sailingCollector)
 
 	http.Handle("/metrics", promhttp.Handler())
+	log.Println("Starting data.gov.gr exporter on port :13211")
 	log.Fatal(http.ListenAndServe(":13211", nil))
 }
